b5611b3aa7e80cd14ec112e7ce98da7c21b1cda0
Michael Pavlov <mihpavlov@gmail.com>
2017-01-25 10:31:00 +0300

    ticket:1 south to settings

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -42,7 +42,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # local apps
     'apps.hello',
+
+    # third party apps
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
