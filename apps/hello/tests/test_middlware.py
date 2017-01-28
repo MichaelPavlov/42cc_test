e30b780eb5c2a60eecc62316924a1053f86a6b26
Michael Pavlov <mihpavlov@gmail.com>
2017-01-28 22:39:45 +0300

    ticket:3 adds test_middlware

---
+++ apps/hello/tests/test_middlware.py
@@ -0,0 +1,24 @@
+from apps.hello.middlware import RequestCaptureMiddlware
+from django.test import RequestFactory
+from django.test import TestCase
+
+from apps.hello.models import RequestStamp
+
+
+class RequestCaptureMiddlwareTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.factory = RequestFactory()
+        cls.rc_middlware = RequestCaptureMiddlware()
+
+    def test_capture_request(self):
+        """
+        Test that RequestCaptureMiddlware captures requests to RequestStamp model
+        """
+        url = "/"
+        request = self.factory.get(url)
+        models_before = RequestStamp.objects.count()
+        self.rc_middlware.process_request(request)
+        models_after = RequestStamp.objects.count()
+
+        self.assertNotEqual(models_after, models_before)
